6219177ca1bca294435fd683abf658cbd9f2010e
Harry Turnbull <hturnbull@live.co.uk>
2020-04-07 17:12:34 +0100

    test inserts data into table before checks

--- spec/boomkark_list_spec.rb
+++ spec/boomkark_list_spec.rb
@@ -1,8 +1,16 @@
 require 'bookmark_list'
+require 'pg'
 
 describe Bookmark do
   describe '.all' do
     it 'displays the users boomarks in a list' do
+      # Connect to test DB
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      # Add the test data
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.destroyallsoftware.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.google.com');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include("http://www.makersacademy.com")
