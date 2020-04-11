112a0c110f5243cc957bf24ec923dda8d3785712
Alexa <alexakearns@outlook.com>
2020-04-11 10:53:02 +0100

    database connection unit test passed - green

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -5,7 +5,6 @@ describe DatabaseConnection do
   describe '.setup' do
     it 'sets up a connection to a database through PG' do
       expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
-
       DatabaseConnection.setup('bookmark_manager_test')
     end
   end
