edb66d2fc2f05dfb7f4a04ab57ac325e8e9dd0c1
Alexa <alexakearns@outlook.com>
2020-04-11 10:56:22 +0100

    unit test for persistent connection

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -7,5 +7,11 @@ describe DatabaseConnection do
       expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
       DatabaseConnection.setup('bookmark_manager_test')
     end
+
+    it 'connection is persistent' do
+      # Grab the connection as a return value from the .setup method
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      expect(DatabaseConnection.connection).to eq connection
+    end
   end
 end
